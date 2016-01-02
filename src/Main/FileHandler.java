6353bed36e553c57837d776cefb16391dd854478
AllTheHotkeys <ally@hvitt.co.uk>
2016-01-02 23:07:26 +0000

    Started on file hanmdling

--- src/Main/FileHandler.java
+++ src/Main/FileHandler.java
@@ -1,6 +1,11 @@
 package Main;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class FileHandler {
+	
 
 	public static void save() {
 		String filepath = getFileLocation();
@@ -8,8 +13,8 @@ public class FileHandler {
 	}
 	
 	public static String getFileLocation(){
-		
-		
+		JFileChooser jFileChooser = new JFileChooser();
+		jFileChooser.setCurrentDirectory(new File("/User/" + System.getProperty("user.name")));
 		
 		
 		
